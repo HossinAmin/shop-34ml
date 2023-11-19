5ca87c978ccfe69989ed02248e8edb07ff3fdefb
HossinAmin <hossinamin@diverse.dev>
2023-11-19 20:56:04 +0200

    create nav bar

--- src/components/common/iconButton.vue
+++ src/components/common/iconButton.vue
@@ -3,7 +3,7 @@ defineProps(["icon", "text"]);
 </script>
 
 <template>
-  <div class="flex gap-2 items-center">
+  <div class="flex gap-1 items-center">
     <img class="w-6 aspect-square" :src="icon" :alt="text" />
     <p>{{ text }}</p>
   </div>
---
+++ src/components/topbar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import heartIcon from "~/assets/icons/heart.svg";
+import cartIcon from "~/assets/icons/cart.svg";
+import accountIcon from "~/assets/icons/account.svg";
+
+import iconButton from "~/components/common/iconButton.vue";
+
+const headerButtons = [
+  {
+    text: "Account",
+    icon: accountIcon,
+  },
+  {
+    text: "Wishlist",
+    icon: heartIcon,
+  },
+  {
+    text: "Cart",
+    icon: cartIcon,
+  },
+];
+</script>
+
+<template>
+  <div class="w-full flex justify-between px-14 py-5">
+    <p class="text-3xl font-bold">LOGO</p>
+    <div class="flex gap-6">
+      <iconButton
+        v-for="headerButton in headerButtons"
+        :text="headerButton.text"
+        :icon="headerButton.icon"
+      />
+    </div>
+  </div>
+</template>
