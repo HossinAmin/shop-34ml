7148be5e90236e0297794d4831856f7af01b4b94
HossinAmin <hossinamin@diverse.dev>
2023-11-19 23:13:02 +0200

    create search bar

--- src/components/sidebar/filters/brand.vue
+++ src/components/sidebar/filters/brand.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchBar from "~/components/common/searchBar.vue";
+
+import { ref } from "vue";
+const searchQuery = ref("");
+</script>
 
 <template>
-  <h3 class="text-text font-bold text-xl">Brand</h3>
+  <div class="flex flex-col gap-3">
+    <h3 class="text-text font-bold text-xl">Brand</h3>
+    <SearchBar
+      v-model:model-value="searchQuery"
+      placeholder="Search by brand"
+    />
+  </div>
 </template>
--- src/components/sidebar/filters/category.vue
+++ src/components/sidebar/filters/category.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchBar from "~/components/common/searchBar.vue";
+
+import { ref } from "vue";
+const searchQuery = ref("");
+</script>
 
 <template>
   <div class="flex flex-col gap-3">
     <h3 class="text-text font-bold text-xl">Category</h3>
+    <SearchBar
+      v-model:model-value="searchQuery"
+      placeholder="Search by category"
+    />
   </div>
 </template>
