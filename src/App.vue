8424891a9be4ec00c5ec0c213efd1bfe38d165d7
HossinAmin <hossinamin@diverse.dev>
2023-11-20 15:43:55 +0200

    create grid component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import breadcrum from "./components/breadcrum.vue";
 import navbar from "./components/navbar.vue";
 import topbar from "./components/topbar.vue";
 import sidebar from "./components/sidebar/index.vue";
+import ProductGrid from "./components/product/grid.vue";
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import sidebar from "./components/sidebar/index.vue";
     <breadcrum />
     <main class="flex">
       <sidebar />
+      <ProductGrid />
     </main>
   </div>
 </template>
---
+++ src/components/product/grid.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import useProducts from "~/composables/useProducts";
+import ProductGridItem from "./gridItem.vue";
+
+const { products, fetchProducts } = useProducts();
+fetchProducts();
+</script>
+
+<template>
+  <div v-if="products" class="flex flex-wrap gap-9 justify-center">
+    <product-grid-item v-for="product in products.data" :product="product" />
+  </div>
+</template>
