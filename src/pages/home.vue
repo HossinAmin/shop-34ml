bf904799a3776d78495162b0a76ca1ccc22dd36a
HossinAmin <hossinamin@diverse.dev>
2023-11-20 22:21:14 +0200

    add vue router

---
+++ src/pages/home.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import breadcrum from "~/components/breadcrum.vue";
+import navbar from "~/components/navbar.vue";
+import topbar from "~/components/topbar.vue";
+import sidebar from "~/components/sidebar/index.vue";
+import ProductsGrid from "~/components/product/grid.vue";
+import paginationButtonsGroup from "~/components/pagination/buttonsGroup.vue";
+</script>
+
+<template>
+  <div class="w-full flex flex-col">
+    <topbar />
+    <navbar />
+    <breadcrum />
+    <main class="flex px-9 gap-5">
+      <sidebar />
+      <products-grid />
+    </main>
+    <footer class="py-5">
+      <pagination-buttons-group />
+    </footer>
+  </div>
+</template>
