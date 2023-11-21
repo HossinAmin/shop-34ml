7bbb0dbd883d30e34decfef2c2bba930c1a8133c
HossinAmin <hossinamin@diverse.dev>
2023-11-22 01:47:03 +0200

    create filter bar card item

---
+++ src/components/filterbarItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+import closeIcon from "~/assets/icons/close.svg";
+import { Brand, Category } from "~/types/response";
+import IconButton from "./common/iconButton.vue";
+
+defineProps({
+  item: {
+    type: Object as PropType<Category | Brand>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="bg-surface rounded flex justify-center items-center gap-2 p-2">
+    <span>{{ item.title }}</span>
+
+    <icon-button @click="$emit('remove')" :icon="closeIcon" />
+  </div>
+</template>
