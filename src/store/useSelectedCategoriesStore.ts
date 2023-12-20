293431b989a2043a943f46d608a437130d10feaa
HossinAmin <hossinamin70gmail.com>
2023-12-20 13:19:46 +0200

    create selectedCategoriesStore

---
+++ src/store/useSelectedCategoriesStore.ts
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+import { Category } from "~/types/response";
+
+export const useSelectedCategoriesStore = defineStore("selected-categories", {
+  state: () => ({
+    selectedCategories: [] as Category[],
+  }),
+  getters: {
+    selectedCategoriesLength: (state) => state.selectedCategories.length,
+  },
+  actions: {
+    addCategory(category: Category) {
+      this.selectedCategories.push(category);
+    },
+    removeCategory(category: Category) {
+      this.selectedCategories = this.selectedCategories.filter(
+        (item) => item.id !== category.id
+      );
+    },
+  },
+});
